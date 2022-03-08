52c31ea92675ceacb64b2fef773fab3de76ae340
evan <ejeffries14@gmail.com>
2022-03-07 19:17:17 -0500

    Done.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomesController < ApplicationController
+
+    def new
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resources :user, only: [:new, :create]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/welcome', to: 'welcome#new'
+  resources :users, only: [:new, :create]
 end
